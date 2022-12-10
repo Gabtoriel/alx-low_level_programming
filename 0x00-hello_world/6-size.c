1859761be51276c37c067cae2d49e6825760ab50
Viclins_Toriel <gabtorielvix@gmail.com>
2022-12-10 10:37:28 -0800

    A program that displays the size of various c types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program to print the * sizes of memory contained by types
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int k;
+	long long int v;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)(sizeof(c)));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)(sizeof(i)));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)(sizeof(k)));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)(sizeof(v)));
+	return (0);
+}
