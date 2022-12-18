cfc98d3e2ac9d6fd89fbb8b092b3e0c30e685af3
Viclins_Toriel <gabtorielvix@gmail.com>
2022-12-18 13:48:10 -0800

    made a little change

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,29 +8,18 @@ void jack_bauer(void)
 {
 	int i;
 
-	for (i = 0; i <= 2; i++)
+	for (i = 0; i < 24; i++)
 	{
 		int j;
 
-		for (j = 0; j <= 3; j++)
+		for (j = 0; j < 60; j++)
 		{
-			int k;
-
-			for (k = 0; k <= 5; k++)
-			{
-				int l;
-
-				for (l = 0; l <= 9; l++)
-				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
