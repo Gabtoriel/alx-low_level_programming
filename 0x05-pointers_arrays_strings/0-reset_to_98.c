6987d1e95060b8796829951c197e5392a51efd94
Viclins_Toriel <gabtorielvix@gmail.com>
2022-12-19 14:47:38 -0800

    program that uses a point to change a global variable from a function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this uses a pointer to change the value of a global variable
+ * @n: a pointer to the global variable passed to the function
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
