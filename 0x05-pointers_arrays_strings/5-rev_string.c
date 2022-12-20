1c5ac21d71b717ea528c30153a490460a8fd8f91
Viclins_Toriel <gabtorielvix@gmail.com>
2022-12-20 08:49:27 -0800

    program to reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a given string in reverse
+ * @s: pointer to a string
+ */
+void rev_string(char *s)
+{
+	int i, j, k, count, r_count;
+	char r_s[count];
+
+	i = 0;
+	count = 0;
+
+	while ((*(s + i)) != '\0')
+	{
+		count++;
+		i++;
+	}
+
+	for (j = 0; j < count; j++)
+	{
+		r_s[j] = *(s + j);
+	}
+
+	r_count = count;
+	--count;
+	for (k = 0; k < r_count; k++)
+	{
+		*(S + K) = r_s[count];
+		--count;
+	}
+}
