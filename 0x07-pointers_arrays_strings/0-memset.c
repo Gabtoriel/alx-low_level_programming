8a6f44fb2c9715ae7c9516399efff2e7dbfb44d8
Viclins_Toriel <gabtorielvix@gmail.com>
2023-01-02 13:48:24 -0800

    program to copy a byte to a memory area

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - program to set n bytes of an array to a particular byte
+ * @s: array to set its byte
+ * @b: byte const to set in array index
+ * @n: no of array elements to set
+ * Return: (*ptr) to the array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Even more pointers arrays and strings
+project directory that contains code
+about copying bytes and finding strings from one memory location to the other
