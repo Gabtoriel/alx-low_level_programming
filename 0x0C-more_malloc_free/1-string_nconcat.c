91db899770dcec0efb0739e106384e47b59d8969
Viclins_Toriel <gabtorielvix@gmail.com>
2023-01-15 12:08:18 -0800

    just made a null character as condotion so the string can be counted properly

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,10 +19,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[l1])
+	while (s1[l1] != '\0')
 		l1++;
 
-	while (s2[l2])
+	while (s2[l2] != '\0')
 		l2++;
 
 	if (n > l2 || n == l2)
