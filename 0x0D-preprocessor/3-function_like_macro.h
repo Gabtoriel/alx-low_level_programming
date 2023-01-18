d8337d859fb4bc255f0f23601f729a2d06cf3094
Viclins_Toriel <gabtorielvix@gmail.com>
2023-01-18 04:30:06 -0800

    My preprocessor and macros project

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,1 +1,6 @@
-#define ABS(x) ((x) > (0) ? (x) : (-x))
+#ifndef _FUNCTION_LIKE_MACRO_H
+#define _FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) (((x) >= 0) ? (x) : -(x))
+
+#endif /* _FUNCTION_LIKE_MACRO_H */
