6b81478ae33d3bbb8c5f788aeed9baae5fe57f74
Gabtoriel <gabtorielvixgmail.com>
2023-07-12 13:43:51 +0000

    program to implement a linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef __MAIN_SEARCH_H__
+#define __MAIN_SEARCH_H__
+
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* __MAIN_SEARCH_H__ */
